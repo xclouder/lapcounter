8e55a882f47eec0e52826bde6c96ac49a9d8191d
xClouder <xclouder@qq.com>
2018-07-22 12:18:18 +0800

    init commit

---
+++ Assets/LapCounter/LapCounter.cs
@@ -0,0 +1,212 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 圈数计数器，用于计算局内排名。设计此计数器来支持非触发器方式的路点更新器
+/// 分两种更新策略，Local和Remote，当本地有更新的时候，以本地为主；
+/// 假设一直有Remote更新，用于确定本地更新时的正确圈数
+/// 断线重连的情况直接使用Reset接口
+/// </summary>
+public class LapCounter {
+
+	public enum UpdateStrategy
+	{
+		PreferLocal,
+		PreferRemote,
+	}
+
+	private UpdateStrategy _strategy;
+
+	private int _totalLapCnt;
+	private int _waypointsCntPerLap;
+
+	private int _curLapCnt;
+	private int _curWaypointIdx;
+
+	private int _curLapCntRemote;
+	private int _curWaypointIdxRemote;
+
+	//最大容忍路点误差
+	private const int cMaxWaypointError = 12;
+
+
+	public LapCounter(int lapCount, int waypointsCntPerLap)
+	{
+		_totalLapCnt = lapCount;
+		_waypointsCntPerLap = waypointsCntPerLap;
+		_strategy = UpdateStrategy.PreferLocal;
+	}
+
+	public void SetStrategy(UpdateStrategy s)
+	{
+		_strategy = s;
+	}
+
+	/// <summary>
+	/// 本地路点更新接口，当Local和Remote在接近的时间调用时，以本地为准
+	/// </summary>
+	/// <param name="newWaypoitIdx">New waypoit index.</param>
+	public void UpdateWayPointLocal(int newWaypoitIdx)
+	{
+		if (_strategy != UpdateStrategy.PreferLocal)
+		{
+			return;
+		}
+
+		var originWpIdx = _curWaypointIdx;
+
+		if (newWaypoitIdx < originWpIdx)
+		{
+			if (originWpIdx - newWaypoitIdx > (_waypointsCntPerLap - cMaxWaypointError))
+			{
+				//lap increase
+				if (IsFinished)
+				{
+					Debug.LogWarning("already finished");
+				}
+				else
+				{
+					_curWaypointIdx = newWaypoitIdx;
+					IncreaseLap();
+				}
+			}
+			else
+			{
+				//如果是不同步造成的reset，这时可能remote圈数大于当前圈数，则直接使用remote的圈数
+				if (_curLapCntRemote > _curLapCnt)
+				{
+					//increase lap
+					IncreaseLap();
+				}
+				else
+				{
+					//remote圈数和当前一致的情况，也有可能是预测点过了圈，但remote数据还没来得及同步。也要加圈
+					if (_curWaypointIdxRemote - newWaypoitIdx > (_waypointsCntPerLap - cMaxWaypointError))
+					{
+						_curWaypointIdx = newWaypoitIdx;
+						IncreaseLap();
+					}
+					else
+					{
+						//正常的往回行驶
+						_curWaypointIdx = newWaypoitIdx;
+					}
+				}
+			}
+		}
+		else
+		{
+			if (newWaypoitIdx - originWpIdx > (_waypointsCntPerLap - cMaxWaypointError))
+			{
+				DecreaseLap();
+
+				_curWaypointIdx = newWaypoitIdx;
+			}
+			else
+			{
+				//move forward
+				_curWaypointIdx = newWaypoitIdx;
+			}
+
+		}
+
+		_curWaypointIdx = newWaypoitIdx;
+	}
+
+	/// <summary>
+	/// 远程路点更新接口，当对象被裁剪时更新路点
+	/// </summary>
+	/// <param name="newWaypointIdx">New waypoint index.</param>
+	public void UpdateWayPointRemote(int lapCount, int newWaypointIdx)
+	{
+		_curLapCntRemote = lapCount;
+		_curWaypointIdxRemote = newWaypointIdx;
+
+		if (_strategy == UpdateStrategy.PreferRemote)
+		{
+			_curLapCnt = lapCount;
+			_curWaypointIdx = newWaypointIdx;
+		}
+	}
+
+	/// <summary>
+	/// 只有在断线重连后调用此借口
+	/// </summary>
+	/// <param name="lapCount">Lap count.</param>
+	/// <param name="waypointIndex">Waypoint index.</param>
+	public void Reset(int lapCount, int waypointIndex)
+	{
+		Debug.Log("Reset to lap:" + lapCount + ", waypointIndex:" + waypointIndex);
+
+		_curLapCnt = lapCount;
+		_curWaypointIdx = waypointIndex;
+	}
+
+	public int CurrentLap
+	{
+		get {
+			return _curLapCnt;
+		}
+	}
+
+	public int CurrentWaypointIndex
+	{
+		get {
+			return _curWaypointIdx;
+		}
+	}
+
+	private bool _isFinished;
+	public bool IsFinished
+	{
+		get 
+		{
+			return _isFinished;
+		}
+	}
+
+	private void OnFinished()
+	{
+		Debug.Log("finished.");
+	}
+
+	private void OnDecreaseLap()
+	{
+		Debug.Log("DecreaseLap to:" + _curLapCnt);
+	}
+
+	private void OnIncreaseLap()
+	{
+		Debug.Log("IncreaseLap to:" + _curLapCnt);
+	}
+
+	public void IncreaseLap()
+	{
+		_curLapCnt++;
+
+		OnIncreaseLap();
+
+		if (_curLapCnt >= _totalLapCnt)
+		{
+			_isFinished = true;
+			OnFinished();
+		}
+	}
+
+	public void DecreaseLap()
+	{
+		_curLapCnt--;
+
+		if (_curLapCnt < 0)
+			_curLapCnt = 0;
+		
+		OnDecreaseLap();
+	}
+
+	private int GetScore(int lap, int wpIdx)
+	{
+		return lap * _waypointsCntPerLap + wpIdx;
+	}
+
+
+}
---
+++ Assets/UnitTests/Editor/TestLapCounter.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestLapCounter {
+
+	private const int cLapWaypointCount = 100;
+
+	[Test]
+	public void TestNormalIncreaseDecreaseLap()
+	{
+
+		LapCounter lc = new LapCounter(2, cLapWaypointCount);
+
+
+		for (int i = 0; i <= cLapWaypointCount; i++)
+		{
+			var k = i;
+			if (i == cLapWaypointCount)
+			{
+				k = i - cLapWaypointCount;
+			}
+
+			lc.UpdateWayPointLocal(k);
+
+			if (i < cLapWaypointCount)
+			{
+				Assert.AreEqual(0, lc.CurrentLap);
+			}
+			else
+			{
+				Assert.AreEqual(1, lc.CurrentLap);
+			}
+
+		}
+
+		lc.UpdateWayPointLocal(cLapWaypointCount - 5);
+
+		Assert.AreEqual(0, lc.CurrentLap);
+
+	}
+
+	[Test]
+	public void TestPassiveResetForNetworkDelay()
+	{
+		LapCounter lc = new LapCounter(2, cLapWaypointCount);
+
+		for (int i = 0; i <= cLapWaypointCount / 2; i++)
+		{
+			var k = i;
+			if (i == cLapWaypointCount)
+			{
+				k = i - cLapWaypointCount;
+			}
+
+			lc.UpdateWayPointLocal(k);
+		}
+
+		Assert.AreEqual(0, lc.CurrentLap);
+
+		//继续同步remote一段时间，但本地模拟碰撞等情况，导致不更新路点
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex - 2);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex + 3);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex + 10);
+		lc.UpdateWayPointRemote(0, cLapWaypointCount - 5);
+		lc.UpdateWayPointRemote(0, cLapWaypointCount - 3);
+
+		//这一行可注释，模拟同步数据的圈数还没+1，但预测后reset导致+1的情况。效果见TestPassiveResetForNetworkDelay2
+		lc.UpdateWayPointRemote(1, 1);
+
+		//一段时间后被动reset，直接跳跃式更新路点
+		lc.UpdateWayPointLocal(2);
+		Assert.AreEqual(1, lc.CurrentLap);
+
+	}
+
+	[Test]
+	public void TestPassiveResetForNetworkDelay2()
+	{
+		LapCounter lc = new LapCounter(2, cLapWaypointCount);
+
+		for (int i = 0; i <= cLapWaypointCount / 2; i++)
+		{
+			var k = i;
+			if (i == cLapWaypointCount)
+			{
+				k = i - cLapWaypointCount;
+			}
+
+			lc.UpdateWayPointLocal(k);
+		}
+
+		Assert.AreEqual(0, lc.CurrentLap);
+
+		//继续同步remote一段时间，但本地模拟碰撞等情况，导致不更新路点
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex - 2);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex + 3);
+		lc.UpdateWayPointRemote(0, lc.CurrentWaypointIndex + 10);
+		lc.UpdateWayPointRemote(0, cLapWaypointCount - 5);
+		lc.UpdateWayPointRemote(0, cLapWaypointCount - 3);
+
+		//lc.UpdateWayPointRemote(1, 1);
+
+		//一段时间后被动reset，直接跳跃式更新路点
+		lc.UpdateWayPointLocal(2);
+		Assert.AreEqual(1, lc.CurrentLap);
+
+	}
+
+	[Test]
+	public void TestWithNormalRemoteUpdate()
+	{
+		LapCounter lc = new LapCounter(2, cLapWaypointCount);
+
+		//车被裁剪后，启用PreferRemote策略
+		lc.SetStrategy(LapCounter.UpdateStrategy.PreferRemote);
+
+		lc.UpdateWayPointRemote(1, 10);
+
+		Assert.AreEqual(1, lc.CurrentLap);
+		Assert.AreEqual(10, lc.CurrentWaypointIndex);
+	}
+
+	[Test]
+	public void TestSwitchStragegyInCriticalSituation()
+	{
+		//切换策略的时候，距离比较远用户一般无感知。咱不处理
+		Assert.Pass();
+	}
+
+	[Test]
+	public void TestFinish()
+	{
+		LapCounter lc = new LapCounter(3, cLapWaypointCount);
+
+		for (int i = 0; i <= cLapWaypointCount * 3; i++)
+		{
+			var k = i % cLapWaypointCount;
+
+			lc.UpdateWayPointLocal(k);
+		}
+
+		Assert.AreEqual(3, lc.CurrentLap);
+		Assert.IsTrue(lc.IsFinished);
+	}
+
+	/// <summary>
+	/// 测试网络车重连，重置其位置的情况
+	/// </summary>
+	[Test]
+	public void TestNetKartReconnect()
+	{
+		LapCounter lc = new LapCounter(3, cLapWaypointCount);
+
+
+		for (int i = 0; i <= cLapWaypointCount; i++)
+		{
+			var k = i;
+			if (i == cLapWaypointCount)
+			{
+				k = i - cLapWaypointCount;
+			}
+
+			lc.UpdateWayPointLocal(k);
+		}
+
+		lc.Reset(2, 8);
+
+		Assert.AreEqual(2, lc.CurrentLap);
+
+		lc.UpdateWayPointLocal(10);
+
+		Assert.AreEqual(2, lc.CurrentLap);
+		Assert.AreEqual(10, lc.CurrentWaypointIndex);
+	}
+}
